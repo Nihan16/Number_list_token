c4b6bd71213b9d3648b32db2525bf45cfd2e95df
Nihan16 <152619151+Nihan16@users.noreply.github.com>
2025-07-17 13:23:13 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,7 @@
-from config import NUMBERS_FILE
-
-def get_next_numbers(start, count):
+def get_next_numbers(start: int, count: int):
     try:
-        with open(NUMBERS_FILE, "r", encoding="utf-8") as f:
-            all_numbers = [line.strip() for line in f if line.strip()]
-        return all_numbers[start:start+count]
+        with open("numbers.txt", "r", encoding="utf-8") as f:
+            lines = f.read().splitlines()
+            return lines[start:start + count]
     except FileNotFoundError:
         return []
