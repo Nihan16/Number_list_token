cf926ba24dda6dfa0e7255a572df7fb1356ae0c7
Nihan16 <152619151+Nihan16@users.noreply.github.com>
2025-07-19 00:15:11 +0600

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-def get_next_numbers(start: int, count: int):
-    try:
-        with open("numbers.txt", "r", encoding="utf-8") as f:
-            lines = f.read().splitlines()
-            return lines[start:start + count]
-    except FileNotFoundError:
-        return []
